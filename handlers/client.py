521bb1f7fdf7d97a6161799132866a592b8d0e1a
LaskiTaski <dgj.vap.11@mail.ru>
2023-05-11 23:18:46 +0300

    Git appending!last commit Help me

--- handlers/client.py
+++ handlers/client.py
@@ -16,23 +16,24 @@ class FSMClient(StatesGroup):
 async def cb_price(callback: types.CallbackQuery, state : FSMContext):
     price_kb = types.InlineKeyboardMarkup(row_width=1)
     price_kb.add(cancel, back)
-    await state.update_data(id_one=callback.message.message_id)
+    await state.update_data(ID=callback.message.message_id)
     await state.set_state(FSMClient.full_price_CNY.state)
     await callback.message.edit_text(f'[Введите Цену товара в CNY]',
                                      reply_markup=price_kb)
 
 
 #Выход из состояний
-# @dp.callback_query_handler(state='*', commands='отмена')
-# @dp.callback_query_handler(Text(equals='отмена', ignore_case=True), state='*')
+# @dp.callback_query_handler(text='отмена', state='*')
 async def cancel_handler(callback: types.CallbackQuery, state: FSMContext):
     cancel_kb = types.InlineKeyboardMarkup(row_width=1)
-    cancel_kb.add(cancel, back)
+    cancel_kb.add(new, back)
+
     current_state = await state.get_state()
     if current_state is None:
         return
-    await state.finish()
     await callback.answer(text='Данные удалены',show_alert=True)
+    await callback.message.edit_text(text='Данные удалены',reply_markup=cancel_kb)
+    await state.finish()
 
 
 # ЖДЁМ Кол-во позиций
@@ -40,18 +41,18 @@ async def cancel_handler(callback: types.CallbackQuery, state: FSMContext):
 async def quantity(message : types.Message, state : FSMContext):
     quantity_kb = types.InlineKeyboardMarkup(row_width=1)
     quantity_kb.add(cancel, back)
-    await state.update_data(CNY_text=message.text)
+
+    await state.update_data(CNY=message.text)
     await message.delete()
     await FSMClient.next()
-    await state.update_data(id_two=message.message_id)
     user_data = await state.get_data()
-    id_one = user_data['id_one']
-    CNY_text = user_data['CNY_text']
+    ID = user_data['ID']
+    CNY = user_data['CNY']
     await bot.edit_message_text(
                                 chat_id=message.chat.id,
-                                message_id=id_one,
+                                message_id=ID,
                                 text=f'[А теперь введите кол-во позиций:\n'
-                                     f'Цена товара: {CNY_text}]',
+                                     f'Цена товара: {CNY}]',
                                 reply_markup=quantity_kb)
 
 
@@ -61,26 +62,27 @@ async def calculation(message: types.Message, state: FSMContext):
     calculation_kb = types.InlineKeyboardMarkup(row_width=1)
     calculation_kb.add(cancel, back)
 
-    await state.update_data(POS_text=message.text)
-
+    await state.update_data(POS=message.text)
+    current = message.chat.id
     await message.delete()
     await FSMClient.next()
     user_data = await state.get_data()
-    id_two = user_data['id_two']
-    POS_text = user_data['POS_text']
+    ID = user_data['ID']
+    CNY = user_data['CNY']
+    POS = user_data['POS']
     await bot.edit_message_text(
-                                chat_id=message.chat.id,
-                                message_id=id_two,
-                                text=f'[А т\n'
-                                     f'Цена товара: {POS_text}]',
+                                chat_id=current,
+                                message_id=ID,
+                                text=f'[Вы указали:\n'
+                                     f'Стоимость : {CNY}\n'
+                                     f'Кол-во : {POS}\n'
+                                     f'Общая стоимость {int(CNY)*int(POS)}]',
                                 reply_markup=calculation_kb)
-
-
+# (цена+22+30 * кол-во позиций)*курс + (кол-поз*1000)
 
 def register_handlers_client(dp : Dispatcher):
     dp.register_callback_query_handler(cb_price, text='price', state=None)
-    dp.register_callback_query_handler(cancel_handler, state='*', text='отмена')
-    dp.register_callback_query_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state='*')
+    dp.register_callback_query_handler(cancel_handler, text='отмена', state='*')
     dp.register_message_handler(quantity, state=FSMClient.full_price_CNY)
     dp.register_message_handler(calculation, state=FSMClient.full_position)
 
