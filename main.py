b3166b17bf4481b9ccfa805e93012c48f5a6d232
LaskiTaski <dgj.vap.11@mail.ru>
2023-05-14 19:13:51 +0300

    last commit?

--- main.py
+++ main.py
@@ -12,9 +12,3 @@ client.register_handlers_client(dp)
 if __name__ == '__main__':
     print('Работаем!')
     executor.start_polling(dp, skip_updates=True)
-
-
-
-# git remote add origin https://github.com/LaskiTaski/AnReyMagazine.git
-#  git branch -M main
-# git push -u origin main
